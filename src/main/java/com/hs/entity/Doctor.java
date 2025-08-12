063b2580edefeebf2aad4b9ed1115fcfef53826c
srinivasbadiga2808 <srinivasbadiga2808@gmail.com>
2025-08-12 15:21:06 -0400

    Create Doctor.java

---
+++ src/main/java/com/hs/entity/Doctor.java
@@ -0,0 +1,31 @@
+package com.example.hs.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "doctors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Doctor {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String firstName;
+
+  @Column(nullable = false)
+  private String lastName;
+
+  @Column(nullable = false, unique = true)
+  private String licenseNumber;
+
+  private String specialty;
+
+  private String phone;
+}
